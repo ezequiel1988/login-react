005ab699def056551ef635973716b1e2ecbeac5a
leo_depmerlo@hotmail.com <leo_depmerlo@hotmail.com>
2020-01-11 10:42:55 -0300

    cambios en el ruteo

--- src/store/userLoginStore.js
+++ src/store/userLoginStore.js
@@ -7,22 +7,56 @@ class userLoginStore {
     rest = new Providers()
 
     constructor(){
+        this.nombre="";
+        this.apellido="";
         this.email= "";
         this.password="";
         this.userToken= null;
+        this.showHome = false;
     }
 
-    async loginPost () {
-
-       return this.rest.postLogin(this.email, this.password);
-       
+   async loginPost () {
+       console.log(this.email, this.password)
+        try {
+            const { data } = await this.rest.postLogin(this.email, this.password);
+            console.log(data)
+            this.userToken = data.token;
+            console.log(this.userToken);
+          } catch (error) {
+            console.log(error)
+          }    
     }
+
+   async userPost () {
+       try {
+
+        const { data } = await this.rest.postUser(this.nombre, this.apellido, this.email, this.password);
+        console.log(data)
+           
+       } catch (error) {
+           console.log(error)
+       }
+   }
+
+   async loginDelete () {
+       try {
+
+        const { data } = await this.rest.deleteLogin(this.userToken, this.email, this.password);
+        console.log(data)
+           
+       } catch (e) {
+           console.log(e)
+       }
+   }
 }
 
 decorate(userLoginStore, {
+    nombre:observable,
+    apellido:observable,
     email:observable,
     password:observable,
-    userToken:observable
+    userToken:observable,
+    showHome:observable,
 })
 
 const UserLoginStore = new userLoginStore();
